83474c657e75372ff76723d07c05bea1a9ce4afe
Corbin Halliwill <corbin.halliwill@berkeley.edu>
2014-12-22 21:38:30 -0800

    cleanup files and remove unused views.

--- meteor_lumos/lib/router.js
+++ meteor_lumos/lib/router.js
@@ -13,21 +13,12 @@ Router.map(function() {
     });
 
     this.route('friendPage', {
-    	path: '/friends/:_id',
+    	path: '/users/:_id',
     	data: function() { return Friends.findOne(this.params._id); }
     });
 
     this.route('profile', {
 	path: '/profile'
-    });
-
-    this.route('postEdit', {
-	path: '/posts/:_id/edit',
-	data: function() { return Posts.findOne(this.params._id); }
-    });
-
-    this.route('postSubmit', { 
-	path: '/submit'
     });
 
 });
