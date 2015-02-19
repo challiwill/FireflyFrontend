d41f17e574fe7af28cab4f244a2c5cf1012d179c
Corbin Halliwill <corbin.halliwill@berkeley.edu>
2015-02-18 17:48:19 -0800

    Add berkeley addresses to mongodb. Modify node-dbf to read floats instead of ints.

--- meteor_lumos/server/address_fixtures.js
+++ meteor_lumos/server/address_fixtures.js
@@ -2,25 +2,27 @@ var Parser = Npm.require('node-dbf');
 
 if (Addresses.find().count() == 0) {
 
-    order = ['streetNum','streetName','streetSufx','city','state','zip','lat','lng'];
-
     // TODO would like to make this a relative path somehow
     var parser = new Parser('/Users/corbin/Documents/Projects/FireFly/meteor_lumos/server/dbase/berkeley.dbf');
 
     parser.on('start', function(p) {
-	console.log('Creating mongodb document of Berkeley addresses.');
+	console.log('Populating mongodb document of Berkeley addresses.');
     });
     
     parser.on('header', function(h) {
 	console.log('dBase file header has been parsed');
     });
-
-    // TODO put in address collection
-    parser.on('record', function(record) {
-	console.log('Address: ' + record.StreetNum + ' ' 
-		    + record.StreetName + ' ' 
-		    + record.StreetSufx);
-    });
+    var count = 0;
+    parser.on('record', Meteor.bindEnvironment(function(record){
+	Addresses.insert({'streetNum': record.StreetNum,
+			  'streetName': record.StreetName,
+			  'streetSufx': record.StreetSufx,
+			  'city': record.City,
+			  'state': record.State,
+			  'zip': record.Zip,
+			  'lat': record.latitude,
+			  'lng': record.longitude});
+    }, function(error) {console.log( error);}));
 
     parser.on('end', function(p) {
 	console.log('Finished parsing the dBase file');
